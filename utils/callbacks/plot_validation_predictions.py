9b2c32e3ad67f02130cf21513386d4d30de9833a
martinwhl <asteroid02@126.com>
2021-05-26 14:50:17 +0800

    feat: send email when finished

--- utils/callbacks/plot_validation_predictions.py
+++ utils/callbacks/plot_validation_predictions.py
@@ -1,14 +1,11 @@
 import torch
-import numpy as np
 import matplotlib.pyplot as plt
 from utils.callbacks.base import BestEpochCallback
 
 
 class PlotValidationPredictionsCallback(BestEpochCallback):
     def __init__(self, monitor="", mode="min"):
-        super(PlotValidationPredictionsCallback, self).__init__(
-            monitor=monitor, mode=mode
-        )
+        super(PlotValidationPredictionsCallback, self).__init__(monitor=monitor, mode=mode)
         self.ground_truths = []
         self.predictions = []
 
@@ -16,12 +13,8 @@ class PlotValidationPredictionsCallback(BestEpochCallback):
         self.ground_truths.clear()
         self.predictions.clear()
 
-    def on_validation_batch_end(
-        self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx
-    ):
-        super().on_validation_batch_end(
-            trainer, pl_module, outputs, batch, batch_idx, dataloader_idx
-        )
+    def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
+        super().on_validation_batch_end(trainer, pl_module, outputs, batch, batch_idx, dataloader_idx)
         if trainer.current_epoch != self.best_epoch:
             return
         self.ground_truths.clear()
